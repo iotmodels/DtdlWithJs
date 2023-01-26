f28310ed705006ca614efeac22133cc11eaeac4f
Rido <rido-min@users.noreply.github.com>
2023-01-25 17:41:46 -0800

    reorder comps

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ info.properties.forEach(p => applog('p:', p.name))
 info.commands.forEach(c => applog('c:', c.name))
 info.components.forEach(co => {
     applog('co:', co.name, co.schema)
-    info.compoProps(co.schema).forEach(p => applog('  p:', p.name))
     info.compoTels(co.schema).forEach(t => applog('  t:', t.name))
+    info.compoProps(co.schema).forEach(p => applog('  p:', p.name))
     info.compoCmds(co.schema).forEach(c => applog('  c:', c.name))
 })
